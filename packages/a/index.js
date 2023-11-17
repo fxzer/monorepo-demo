fff3692a021c9b5eb40ba7b4c309d9e4a48073c7
fxzer <fxzer8888@163.com>
2023-11-17 11:16:15 +0800

    feat: ✨ init

---
+++ packages/a/index.js
@@ -0,0 +1,7 @@
+export function funa(){
+  console.log("funa");
+  /**   pnpm i --> ERROR: No matching version found for @demo/a@workspace:1.0.1 inside the workspace
+   *    pnmm up --> workspace:*
+  */
+  console.log("funa version --> 1.0.8"); 
+}
---
+++ packages/b/index.js
@@ -0,0 +1,6 @@
+export function funb(){
+  console.log("funb");
+  console.log("funb versiont ---> 1.0.3");
+  console.log("funb versiont ---> 1.0.4");
+  console.log("funb versiont ---> 1.0.5"); /** 软连接过去，改完后 C 模块不需要升级依赖 */
+}
